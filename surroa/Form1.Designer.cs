7307fd016e1c15c820229678c291da79365c2f31
pathefone <mtvlukas@gmail.com>
2021-09-21 11:57:11 +0300

    Average resource calculations

---
+++ surroa/Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿
+namespace surroa
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SYNC_BUTTON = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // SYNC_BUTTON
+            // 
+            this.SYNC_BUTTON.Location = new System.Drawing.Point(495, 54);
+            this.SYNC_BUTTON.Name = "SYNC_BUTTON";
+            this.SYNC_BUTTON.Size = new System.Drawing.Size(101, 39);
+            this.SYNC_BUTTON.TabIndex = 0;
+            this.SYNC_BUTTON.Text = "SYNC";
+            this.SYNC_BUTTON.UseVisualStyleBackColor = true;
+            this.SYNC_BUTTON.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(495, 132);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(101, 39);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "START";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(495, 212);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(101, 39);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "STOP";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "AV_IDL CPU: ";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 104);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "AV_IDL MEM:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 144);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 15);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "AV_IDL I/O: ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 186);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(70, 15);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "AV_IDL NET:";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(495, 294);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(101, 39);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "EXIT";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(93, 66);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 15);
+            this.label5.TabIndex = 8;
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(95, 104);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 15);
+            this.label6.TabIndex = 9;
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(139, 144);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(0, 15);
+            this.label7.TabIndex = 10;
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(95, 144);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(44, 15);
+            this.label8.TabIndex = 11;
+            this.label8.Text = "Reads: ";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(279, 144);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(43, 15);
+            this.label9.TabIndex = 12;
+            this.label9.Text = "Writes:";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(328, 144);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(0, 15);
+            this.label10.TabIndex = 13;
+            this.label10.Click += new System.EventHandler(this.label10_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.SYNC_BUTTON);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button SYNC_BUTTON;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+    }
+}
+
---
+++ surroa/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace surroa
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+            //READS
+        }
+
+        private void label10_Click(object sender, EventArgs e)
+        {
+            //WRITES
+        }
+
+        PerformanceCounter cpuCounter;
+        PerformanceCounter ramCounter;
+        private PerformanceCounter diskReadsPerformanceCounter = new PerformanceCounter();
+        private PerformanceCounter diskWritesPerformanceCounter = new PerformanceCounter();
+        private PerformanceCounter diskTransfersPerformanceCounter = new PerformanceCounter();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //CPU
+            cpuCounter = new PerformanceCounter("Processor", "% Processor Time", "_Total");
+            label5.Text = "Calculating..";
+            Refresh();
+            Thread.Sleep(2000);
+            cpuCounter.NextValue();
+            Thread.Sleep(1000);
+
+
+            int time = 15;
+            int[] average = new int[15];
+
+            while (time != 0)
+            {
+                int cpu_value = (int)cpuCounter.NextValue();
+                average[time - 1] = cpu_value;
+                time--;
+                Thread.Sleep(1000);
+
+            }
+            int average_cpu = (int)average.Average();
+
+            label5.Text = average_cpu + "%";
+            time = 15;
+
+            //RAM
+            ramCounter = new PerformanceCounter("Memory", "Available MBytes");
+            label6.Text = "Measuring RAM usage...";
+            Refresh();
+            Thread.Sleep(1000);
+            ramCounter.NextValue();
+            Thread.Sleep(1000);
+
+            Array.Clear(average, 0, average.Length);
+
+            while (time != 0)
+            {
+                int ram_value = (int)ramCounter.NextValue();
+                average[time - 1] = ram_value;
+                time--;
+                Thread.Sleep(1000);
+            }
+            int average_ram = (int)average.Average();
+            label6.Text = average_ram + "MB";
+            time = 15;
+
+            //I/O
+
+            //fix to make average
+            //enable multithreading
+
+            this.diskReadsPerformanceCounter.CategoryName = "PhysicalDisk";
+            this.diskReadsPerformanceCounter.CounterName = "Disk Reads/sec";
+            this.diskReadsPerformanceCounter.InstanceName = "_Total";
+
+            this.diskWritesPerformanceCounter.CategoryName = "PhysicalDisk";
+            this.diskWritesPerformanceCounter.CounterName = "Disk Writes/sec";
+            this.diskWritesPerformanceCounter.InstanceName = "_Total";
+
+
+            label7.Text = "Calculating..";
+            Refresh();
+            Thread.Sleep(300);
+            this.diskReadsPerformanceCounter.NextValue();
+            this.diskWritesPerformanceCounter.NextValue();
+            Thread.Sleep(300);
+
+            Array.Clear(average, 0, average.Length);
+
+            while (time != 0)
+            {
+                int disk_value = (int)this.diskReadsPerformanceCounter.NextValue();
+                average[time - 1] = disk_value;
+                time--;
+                Thread.Sleep(1000);
+            }
+            int average_disk_read = (int)average.Average();
+
+            string currentDiskReads = "Disk reads /s : " + this.diskReadsPerformanceCounter.NextValue().ToString() + Environment.NewLine;
+            string currentDiskWrites = "Disk writes /s : " + this.diskWritesPerformanceCounter.NextValue().ToString() + Environment.NewLine;
+
+
+            label7.Text = "Disk reads /s: " + average_disk_read;
+            label10.Text = currentDiskWrites;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        
+    }
+}
